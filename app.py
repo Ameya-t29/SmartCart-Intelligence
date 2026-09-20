907f57bb3ab2648ad85e409c6fcd1b2c1867240b
Ameya Tamanekar <ameyatamanekar@gmail.com>
2026-09-20 19:45:36 +0530

    Prepare SmartCart for Render deployment

--- app.py
+++ app.py
@@ -316,4 +316,10 @@ def customer(customer_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    import os
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
